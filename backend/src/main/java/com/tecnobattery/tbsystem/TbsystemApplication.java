6bbaab123f3b82ce24ebe53ab66497e4912e6bbf
veyson <luisfernando_cezar@hotmail.com>
2021-02-06 15:43:15 -0300

    tecnobattery | init project backend spring

---
+++ backend/src/main/java/com/tecnobattery/tbsystem/TbsystemApplication.java
@@ -0,0 +1,13 @@
+package com.tecnobattery.tbsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbsystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tecnobattery/tbsystem/TbsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnobattery.tbsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
