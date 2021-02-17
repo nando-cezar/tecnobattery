5233b0c1a39dbcdad19e1bc98f761f33ce3b5022
veyson <luisfernando_cezar@hotmail.com>
2021-02-16 22:54:55 -0300

    tecnobattery | implements service client and provider

--- backend/src/main/java/com/tecnobattery/tbsystem/services/ClientService.java
+++ backend/src/main/java/com/tecnobattery/tbsystem/services/ClientService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.tecnobattery.tbsystem.dto.ClientDTO;
+import com.tecnobattery.tbsystem.entities.Address;
 import com.tecnobattery.tbsystem.entities.Client;
 import com.tecnobattery.tbsystem.repositories.ClientRepository;
 
@@ -20,6 +21,26 @@ public class ClientService {
 
   @Autowired
   private ModelMapper mapper;
+
+  public ClientDTO save(String cnpj, String name, String fantasyName, String phone, String email, String postalCode,
+      String publicPlace, String complement, String neighborhood, String city, String state) {
+    Client client = new Client();
+    Address address = new Address();
+    client.setCnpj(cnpj);
+    client.setName(name);
+    client.setFantasyName(fantasyName);
+    client.setPhone(phone);
+    client.setEmail(email);
+    address.setPostalCode(postalCode);
+    address.setPublicPlace(publicPlace);
+    address.setComplement(complement);
+    address.setNeighborhood(neighborhood);
+    address.setCity(city);
+    address.setState(state);
+    client.setAddress(address);
+
+    return toModel(clientRepository.save(client));
+  }
 
   @Transactional(readOnly = true)
   public List<ClientDTO> findAll() {
--- backend/src/main/java/com/tecnobattery/tbsystem/services/ProviderService.java
+++ backend/src/main/java/com/tecnobattery/tbsystem/services/ProviderService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.tecnobattery.tbsystem.dto.ProviderDTO;
+import com.tecnobattery.tbsystem.entities.Address;
 import com.tecnobattery.tbsystem.entities.Provider;
 import com.tecnobattery.tbsystem.repositories.ProviderRepository;
 
@@ -20,6 +21,26 @@ public class ProviderService {
 
   @Autowired
   private ModelMapper mapper;
+
+  public ProviderDTO save(String cnpj, String name, String fantasyName, String phone, String email, String postalCode,
+      String publicPlace, String complement, String neighborhood, String city, String state) {
+    Provider provider = new Provider();
+    Address address = new Address();
+    provider.setCnpj(cnpj);
+    provider.setName(name);
+    provider.setFantasyName(fantasyName);
+    provider.setPhone(phone);
+    provider.setEmail(email);
+    address.setPostalCode(postalCode);
+    address.setPublicPlace(publicPlace);
+    address.setComplement(complement);
+    address.setNeighborhood(neighborhood);
+    address.setCity(city);
+    address.setState(state);
+    provider.setAddress(address);
+
+    return toModel(providerRepository.save(provider));
+  }
 
   @Transactional(readOnly = true)
   public List<ProviderDTO> findAll() {
