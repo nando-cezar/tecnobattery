ebe77003b1d7dd8382069d3b496e9431950edc5d
veyson <luisfernando_cezar@hotmail.com>
2021-02-21 15:21:47 -0300

    tecnobattery | implments vaçidation boardInput

--- backend/src/main/java/com/tecnobattery/tbsystem/dto/input/BatteryInput.java
+++ backend/src/main/java/com/tecnobattery/tbsystem/dto/input/BatteryInput.java
@@ -1,23 +1,36 @@
 package com.tecnobattery.tbsystem.dto.input;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
 
 public class BatteryInput {
 
   private Long id;
-  @NotBlank
-  @Size(max = 60)
+  @NotBlank(message = "Marca se encontra em branco ou inválido.")
+  @Size(max = 60, message = "Marca deve possuir no máximo 60 caracteres.")
   private String brand;
-  @NotBlank
+  @NotBlank(message = "Modelo se encontra em branco ou inválido.")
   @Size(max = 60)
   private String model;
+  @NotNull(message = "Capacidade é obrigatório em mA.")
+  @Range(min = 100, max = 20000, message = "A capacidade varia entre 100mA e 20000mA.")
   private Integer capacity;
+  @NotNull(message = "Tensão é obrigatório em volts.")
+  @Range(min = 1, max = 10, message = "A tensão varia entre 1v e 10v.")
   private Integer voltage;
+  @NotNull(message = "Diâmetro é obrigatório em mm.")
+  @Range(min = 10, max = 100, message = "O diâmetro varia entre 10mm e 100mm.")
   private Integer diameter;
+  @NotNull(message = "Altura é obrigatório em mm.")
+  @Range(min = 10, max = 100, message = "A altura varia entre 10mm e 100mm.")
   private Integer height;
+  @NotNull(message = "Peso é obrigatório em grama.")
+  @Range(min = 10, max = 2000, message = "O peso varia entre 10g e 2000g.")
   private Integer weight;
-  @NotBlank
+  @NotBlank(message = "URL da imagem se encontra em branco ou inválido.")
   private String imageUrl;
 
   public Long getId() {
