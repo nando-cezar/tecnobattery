ecb7a63007baf0c30e101f6ce34aee43cd263f7b
veyson <luisfernando_cezar@hotmail.com>
2021-03-27 14:52:13 -0300

    Tecnobattery | implments CORS

--- backend/src/main/java/com/tecnobattery/tbsystem/security/config/ApplicationSecurityConfig.java
+++ backend/src/main/java/com/tecnobattery/tbsystem/security/config/ApplicationSecurityConfig.java
@@ -9,8 +9,10 @@ import com.tecnobattery.tbsystem.jwt.token.JwtTokenVerifier;
 import com.tecnobattery.tbsystem.jwt.config.JwtConfig;
 import com.tecnobattery.tbsystem.jwt.filter.JwtUsernameAndPasswordAuthenticationFilter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -36,18 +38,23 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
   private final SecretKey secretKey;
   private final JwtConfig jwtConfig;
 
+  @Autowired
+  private Environment env;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-
+    if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+      http.headers().frameOptions().disable();
+    }
     /*
      * http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()
      * );
      */
-    http.cors().disable();
-    http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.cors().and().csrf().disable();
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     http.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey));
     http.addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class);
-    http.authorizeRequests().antMatchers("/", "index", "/css/*", "/js/*").permitAll().anyRequest().authenticated();
+    http.authorizeRequests().anyRequest().permitAll();
   }
 
   @Override
