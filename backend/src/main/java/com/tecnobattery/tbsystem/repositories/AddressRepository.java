a8e49eca40f8d9fca226a8bc7d9958e18297fa97
veyson <luisfernando_cezar@hotmail.com>
2021-04-07 22:15:46 -0300

    Tecnobattery | refactoring

--- backend/src/main/java/com/tecnobattery/tbsystem/repositories/AddressRepository.java
+++ backend/src/main/java/com/tecnobattery/tbsystem/repositories/AddressRepository.java
@@ -1,5 +1,7 @@
 package com.tecnobattery.tbsystem.repositories;
 
+import java.util.Optional;
+
 import com.tecnobattery.tbsystem.entities.Address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,5 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public interface AddressRepository extends JpaRepository<Address, Long> {
 
+  Optional<Address> searchPostalCode(String cep);
 }
